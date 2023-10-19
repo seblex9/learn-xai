843a18895c13e5bbd2f8b6c3014d7f04fb9c1b7f
seblex9 <seblex@proton.me>
2023-10-20 04:44:25 +0800

    Add new Page component with form and navigation link

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -8,7 +8,10 @@ export function TaskItem({ id, title, complete }: TaskItemProps) {
   return (
     <li className='flex gap1 items-center'>
       <input id={id} type='checkbox' className='cursor-pointer peer' />
-      <label htmlFor={id} className='peer-checked:line-through'>
+      <label
+        htmlFor={id}
+        className='cursor-pointer peer-checked:line-through peer-checked:text-slate-500'
+      >
         {title}
       </label>
     </li>
